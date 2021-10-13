4c86131ba493398783cd7b288aa1b9515db5a1fe
Igor Belousov <imbelousov66@gmail.com>
2021-10-13 10:40:28 +0500

    Sample

---
+++ BenchmarkSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkSample
+{
+    public class BenchmarkSample
+    {
+        public BenchmarkSample()
+        {
+            // Setup your benchmark here
+        }
+
+        [Benchmark]
+        public void Function1()
+        {
+        }
+
+        [Benchmark]
+        public void Function2()
+        {
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkSample>();
+        }
+    }
+}
